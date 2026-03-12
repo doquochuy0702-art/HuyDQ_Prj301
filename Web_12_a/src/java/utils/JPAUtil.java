2c0c47cc86cf49110919ed7dcf4572cf069d48fa
doquochuy0702-art <doquochuy0702@gmail.com>
2026-03-12 10:22:18 +0700

    u

---
+++ Web_12_a/src/java/utils/JPAUtil.java
@@ -0,0 +1,15 @@
+package utils;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("Web_11_aPU");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
